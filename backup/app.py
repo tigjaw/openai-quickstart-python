af11fbce09f1867ce1cc78f9ccdfebf70644cf36
Joshua <tigjaw@gmail.com>
2023-02-27 18:41:47 +0000

    made general purpose client
    The app now lets you type anything as your prompt.
    Provided some troubleshooting and created a batch file that opens a browser and runs the app automatically.

--- backup/app.py
+++ backup/app.py
@@ -1,19 +1,20 @@
 import os
+
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        prompt = request.form["prompt"]
+        animal = request.form["animal"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(prompt),
-            temperature=0,
-            max_tokens=500,
+            prompt=generate_prompt(animal),
+            temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -21,9 +22,14 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(prompt):
-    return """
-prompt: {}
+def generate_prompt(animal):
+    return """Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: {}
 Names:""".format(
-        prompt.capitalize()
+        animal.capitalize()
     )
---
+++ run.bat
@@ -0,0 +1,5 @@
+start chrome http://localhost:5000/
+python -m venv venv
+. venv/scripts/activate
+pip install -r requirements.txt
+flask run
